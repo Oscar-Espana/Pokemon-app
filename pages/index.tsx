cd92d51d6ab845890ffd053af7e763b2e59e8687
Oscar España <espaaoscar@gmail.com>
2022-04-17 18:42:23 -0500

    feat: NextUI - React UI Library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@nextui-org/react";
 import type { NextPage } from "next";
 const HomePage: NextPage = () => {
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <Button color="gradient">Hola Mundo</Button>
     </div>
   );
 };
