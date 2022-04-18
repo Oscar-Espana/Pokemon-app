70ab7f0f650e41675bc16dff282f7b65056e6ccd
Oscar España <espaaoscar@gmail.com>
2022-04-18 09:03:10 -0500

    feat: Generando las 151 páginas de Pokémons

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,16 +1,38 @@
-import React, { FC } from "react";
+import React from "react";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { Layout } from "../../components/layouts";
+import { pokeApi } from "../../api";
+import { Pokemon } from "../../interfaces";
 
 interface Props {
-  pokemon: any;
+  pokemon: Pokemon;
 }
 
-const PokemonPage: FC<Props> = () => {
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
   return (
     <Layout>
-      <h1>Hola</h1>
+      <h1>{pokemon.name}</h1>
     </Layout>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`);
+  return {
+    paths: pokemons151.map((id) => ({ params: { id } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params as { id: string };
+
+  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`);
+  return {
+    props: {
+      pokemon: data,
+    },
+  };
 };
 
 export default PokemonPage;
