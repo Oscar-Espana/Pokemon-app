3c9ab689293345a9b02ccc9885c82fb17d51e792
Oscar España <espaaoscar@gmail.com>
2022-04-17 22:14:28 -0500

    feat:  Cargar de forma estática los Pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage, GetStaticProps } from "next";
+import { pokeApi } from "../api";
 import { Layout } from "../components/layouts";
+
 const HomePage: NextPage = (props) => {
   return (
     <Layout title="Listado de Pokemons">
@@ -19,8 +21,11 @@ const HomePage: NextPage = (props) => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get("/pokemon?limit=151");
   return {
-    props: {},
+    props: {
+      pokemons: data.results,
+    },
   };
 };
 export default HomePage;
